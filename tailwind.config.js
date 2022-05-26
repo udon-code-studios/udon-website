8e263b8a84fded56622fac24b571d0feed822928
leo6liu <leo.battalora@gmail.com>
2022-05-26 01:52:06 -0400

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "new-tegomin": ["New Tegomin"],
+        sans: ["Inter"],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
