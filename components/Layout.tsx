32d8ede261424528c5755af554f143f6787286aa
leo6liu <leo.battalora@gmail.com>
2022-05-26 02:36:22 -0400

    color change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 export default function Layout({ children }: { children: any }) {
   return (
-    <div className="min-h-screen w-full dark:bg-black bg-zinc-100 text-zinc-800 dark:text-zinc-300 flex flex-col justify-between">
+    <div className="min-h-screen w-full dark:bg-black bg-zinc-100 text-zinc-800 dark:text-zinc-100 flex flex-col justify-between">
       <div>
         <Navbar />
         <main>{children}</main>
