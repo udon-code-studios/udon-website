c2b6a21159d87534de8434f21dafb2e5e639e63c
leo6liu <leo.battalora@gmail.com>
2022-05-26 01:52:39 -0400

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Custom404: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Page Not Found</title>
+      </Head>
+
+      {/* page */}
+      <div className="w-full flex flex-col items-center py-32 font-new-tegomin text-5xl">
+        <h1>{"404 - page not found :("}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Custom404;
