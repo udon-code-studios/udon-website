6e88fd3ab7b1f1fc2d7991e48b415216f5ff7bad
leo6liu <leo.battalora@gmail.com>
2022-05-27 21:56:09 -0400

    links

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -17,7 +17,7 @@ const People: NextPage = () => {
         <p className="italic text-quaternary">Because code doesn&apos;t write itself... yet :)</p>
 
         {/* people */}
-        <div className="grid grid-flow-row grid-cols-1 gap-y-10 pt-8 sm:grid-cols-2 md:-mx-20 md:grid-cols-3">
+        <div className="grid grid-flow-row grid-cols-1 gap-y-16 pt-8 sm:grid-cols-2 md:-mx-20 md:grid-cols-3">
           {/* Leo Battalora */}
           <div className="flex w-full flex-col items-center space-y-1 text-secondary dark:text-secondary-dark">
             <div className="w-40 overflow-hidden rounded-2xl">
@@ -36,32 +36,34 @@ const People: NextPage = () => {
             </p>
             <p>
               <span className="font-bold">Resume/CV: </span>
-              <a className="underline" href="/assets/people/resumes/battalora-leo.pdf" target="_blank">
-                PDF
-              </a>
+              <Link href="/assets/people/resumes/battalora-leo.pdf" passHref>
+                <a className="underline duration-200 hover:text-zinc-400 dark:hover:text-zinc-300" target="_blank">
+                  PDF
+                </a>
+              </Link>
               ,{" "}
-              <Link href="http://www.google.com" passHref>
-                <a className="underline" target="_blank">
+              <Link href="/assets/people/resumes/battalora-leo.pdf" passHref>
+                <a className="underline duration-200 hover:text-zinc-400 dark:hover:text-zinc-300" target="_blank">
                   Online
                 </a>
               </Link>
             </p>
             <div className="flex items-center space-x-2 pt-1">
               {/* LinkedIn */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="https://www.linkedin.com/in/leo6" passHref>
+                <a title="LinkedIn" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.PhLinkedinLogoBold className="h-6" />
                 </a>
               </Link>
               {/* GitHub */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="https://github.com/leo6liu" passHref>
+                <a title="GitHub" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilGithubAlt className="h-6" />
                 </a>
               </Link>
               {/* Email */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="mailto:leo.battalora@gmail.com" passHref>
+                <a title="Email" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilEnvelopeAlt className="h-6" />
                 </a>
               </Link>
@@ -73,7 +75,7 @@ const People: NextPage = () => {
             <div className="w-40 overflow-hidden rounded-2xl">
               <Image
                 alt="Joshua"
-                src="/assets/people/images/battalora_leo_1000x1000.jpg"
+                src="/assets/people/images/lion_cub.png"
                 layout="responsive"
                 width="1000"
                 height="1000"
@@ -85,32 +87,18 @@ const People: NextPage = () => {
             </p>
             <p>
               <span className="font-bold">Resume/CV: </span>
-              <a className="underline" href="/assets/people/resumes/battalora-leo.pdf" target="_blank">
-                PDF
-              </a>
-              ,{" "}
-              <Link href="http://www.google.com" passHref>
-                <a className="underline" target="_blank">
-                  Online
-                </a>
-              </Link>
+              N/A
             </p>
             <div className="flex items-center space-x-2 pt-1">
-              {/* LinkedIn */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
-                  <icons.PhLinkedinLogoBold className="h-6" />
-                </a>
-              </Link>
               {/* GitHub */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="https://github.com/Genius6942" passHref>
+                <a title="GitHub" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilGithubAlt className="h-6" />
                 </a>
               </Link>
               {/* Email */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="mailto:leo.battalora@gmail.com">
+                <a title="Email" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilEnvelopeAlt className="h-6" />
                 </a>
               </Link>
@@ -134,32 +122,28 @@ const People: NextPage = () => {
             </p>
             <p>
               <span className="font-bold">Resume/CV: </span>
-              <a className="underline" href="/assets/people/resumes/chen-abel.pdf" target="_blank">
-                PDF
-              </a>
-              ,{" "}
-              <Link href="http://www.google.com" passHref>
-                <a className="underline" target="_blank">
-                  Online
+              <Link href="/assets/people/resumes/chen-abel.pdf" passHref>
+                <a className="underline duration-200 hover:text-zinc-400 dark:hover:text-zinc-300" target="_blank">
+                  PDF
                 </a>
               </Link>
             </p>
             <div className="flex items-center space-x-2 pt-1">
               {/* LinkedIn */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="https://www.linkedin.com/in/abel-chen-75172a1b7" passHref>
+                <a title="LinkedIn" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.PhLinkedinLogoBold className="h-6" />
                 </a>
               </Link>
               {/* GitHub */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="https://github.com/Ramenable" passHref>
+                <a title="GitHub" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilGithubAlt className="h-6" />
                 </a>
               </Link>
               {/* Email */}
-              <Link href="/notes">
-                <a title="Notes" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+              <Link href="mailto:achen383@gatech.edu">
+                <a title="Email" className="duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
                   <icons.UilEnvelopeAlt className="h-6" />
                 </a>
               </Link>
