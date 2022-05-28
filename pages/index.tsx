54554e84a00d620ba280461ec7cc6289de29a27e
Leo B <leo.battalora@gmail.com>
2022-05-27 21:18:09 -0400

    color changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,17 +10,17 @@ const Home: NextPage = () => {
       </Head>
 
       {/* page */}
-      <div className="mx-auto flex w-full max-w-screen-sm flex-col space-y-4 py-12 px-4">
-        <h1 className="pb-3 text-4xl font-semibold">What is this?</h1>
+      <div className="mx-auto flex w-full max-w-screen-sm flex-col space-y-4 py-12 px-4 text-primary dark:text-secondary-focus-dark">
+        <h1 className="pb-3 text-4xl font-semibold text-primary dark:text-primary-dark">What is this?</h1>
         <p>Udon Code Studios (UCS) is a fake software company focused on producing clean and beautiful code.</p>
         {/* <p>
           Our core philosophy is to produce software of the highest quality. That means that even the parts you
           don&apos;t see need to be perfect.
         </p> */}
-        <p className="px-14 text-center italic">
+        <p className="px-10 sm:px-14 text-center italic">
           &quot;For you to sleep well at night, the aesthetic, the quality, has to be carried all the way
           through.&quot;&nbsp;&nbsp;&nbsp;
-          <span>— Steve Jobs</span>
+          <span className="whitespace-nowrap">— Steve Jobs</span>
         </p>
         <p>
           Check out our{" "}
@@ -31,13 +31,13 @@ const Home: NextPage = () => {
           </Link>{" "}
           to see what we&apos;ve been up to. And{" "}
           <Link href="/people">
-            <a className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+            <a className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400">
               meet the team
             </a>
           </Link>{" "}
           behind them before you go.
         </p>
-        <h1 className="pt-8 pb-3 text-4xl font-semibold">Leo Battalora</h1>
+        <h2 className="pt-8 pb-3 text-2xl font-semibold text-primary dark:text-primary-dark">Leo Battalora</h2>
         <p>
           Hello! I&apos;m Leo - the guy behind UCS. I really love udon noodes and occasionally do a bit coding on the
           side.
@@ -57,7 +57,7 @@ const Home: NextPage = () => {
           <Link href="https://github.com/leo6liu" passHref>
             <a
               target="_blank"
-              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400 dark:hover:text-zinc-300"
+              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400"
             >
               GitHub
             </a>
@@ -66,7 +66,7 @@ const Home: NextPage = () => {
           <Link href="https://twitter.com/LBattalora" passHref>
             <a
               target="_blank"
-              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400 dark:hover:text-zinc-300"
+              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400"
             >
               Twitter
             </a>
@@ -75,7 +75,7 @@ const Home: NextPage = () => {
           <Link href="https://www.linkedin.com/in/leo6/">
             <a
               target="_blank"
-              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400 dark:hover:text-zinc-300"
+              className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400"
             >
               LinkedIn
             </a>
@@ -85,7 +85,7 @@ const Home: NextPage = () => {
         <p>
           Mail me at{" "}
           <Link href="mailto:leo.battalora@gmail.com" passHref>
-            <a className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400 dark:hover:text-zinc-300">
+            <a className="font-semibold underline decoration-2 underline-offset-2 duration-200 hover:text-zinc-400">
               leo.battalora@gmail.com
             </a>
           </Link>
@@ -96,13 +96,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-/* <div className="w-40">
-            <Image src="/assets/udon-logo-full-text-square.svg" layout="responsive" width="300" height="300" />
-          </div>
-          <div className="w-40">
-            <Image src="/assets/udon-logo-full-text-square.svg" layout="responsive" width="300" height="300" />
-          </div>
-          <div className="w-40">
-            <Image src="/assets/udon-logo-full-text-square.svg" layout="responsive" width="300" height="300" />
-          </div> */
