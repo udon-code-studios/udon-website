1ae4fbbaa49b17753f43806bb5b01af94357ecd9
Leo B <leo.battalora@gmail.com>
2022-05-27 21:17:46 -0400

    added icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -22,6 +22,17 @@ export function PhNoteBlankBold(props: SVGProps<SVGSVGElement>) {
   );
 }
 
+export function PhLinkedinLogoBold(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 256 256" {...props}>
+      <path
+        fill="currentColor"
+        d="M96 120v56a12 12 0 0 1-24 0v-56a12 12 0 0 1 24 0Zm28 68a12 12 0 0 0 12-12v-28a16 16 0 0 1 32 0v28a12 12 0 0 0 24 0v-28a40 40 0 0 0-58.6-35.4A12 12 0 0 0 112 120v56a12 12 0 0 0 12 12ZM232 44v168a20.1 20.1 0 0 1-20 20H44a20.1 20.1 0 0 1-20-20V44a20.1 20.1 0 0 1 20-20h168a20.1 20.1 0 0 1 20 20Zm-24 4H48v160h160ZM84 96a16 16 0 1 0-16-16a16 16 0 0 0 16 16Z"
+      ></path>
+    </svg>
+  );
+}
+
 export function PhUsersThreeBold(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 256 256" {...props}>
@@ -94,6 +105,17 @@ export function PhXBold(props: SVGProps<SVGSVGElement>) {
       <path
         fill="currentColor"
         d="M208.5 191.5a12 12 0 0 1 0 17a12.1 12.1 0 0 1-17 0L128 145l-63.5 63.5a12.1 12.1 0 0 1-17 0a12 12 0 0 1 0-17L111 128L47.5 64.5a12 12 0 0 1 17-17L128 111l63.5-63.5a12 12 0 0 1 17 17L145 128Z"
+      ></path>
+    </svg>
+  );
+}
+
+export function UilLinkedinAlt(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <path
+        fill="currentColor"
+        d="M17.5 8.999a5.419 5.419 0 0 0-2.565.645A1 1 0 0 0 14 8.999h-4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-5.5a1 1 0 1 1 2 0v5.5a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-7.5a5.507 5.507 0 0 0-5.5-5.5Zm3.5 12h-2v-4.5a3 3 0 1 0-6 0v4.5h-2v-10h2v.703a1 1 0 0 0 1.781.625A3.483 3.483 0 0 1 21 14.5Zm-14-12H3a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-12a1 1 0 0 0-1-1Zm-1 12H4v-10h2ZM5.015 1.542a3.233 3.233 0 1 0-.057 6.457h.028a3.233 3.233 0 1 0 .029-6.457Zm-.029 4.457h-.028a1.222 1.222 0 0 1-1.37-1.228c0-.747.56-1.229 1.427-1.229A1.234 1.234 0 0 1 6.41 4.771c0 .746-.56 1.228-1.425 1.228Z"
       ></path>
     </svg>
   );
