88481982d6250a0c8773b4ee358731e0c8125cb7
leo6liu <leo.battalora@gmail.com>
2022-07-10 20:40:34 -0400

    added riot-api and lol-time projects

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import * as icons from "../../components/icons";
 
 const Projects: NextPage = () => {
+  const link = "https://deno.land/x/riot_api/mod.ts";
   return (
     <>
       <Head>
@@ -16,6 +17,47 @@ const Projects: NextPage = () => {
         <h1 className="text-4xl font-semibold text-primary dark:text-primary-dark">Projects</h1>
         <p className="pb-8 italic text-quaternary">List of projects we&apos;re proud of</p>
         <div className="grid grid-flow-row grid-cols-1 gap-8 px-4 sm:px-10 md:grid-cols-2 md:px-0">
+          {/* LoL Time */}
+          <Link href="https://lol.udon.studio" passHref>
+            <a target="_blank" className="group">
+              <div className="flex items-center space-x-4">
+                <div className="w-1/4 p-2 text-secondary duration-200 group-hover:text-zinc-800 dark:text-secondary-dark dark:group-hover:text-secondary-focus-dark">
+                  <icons.LoLTime />
+                </div>
+                <div className="flex w-3/4 flex-col">
+                  <h3 className="text-lg text-secondary duration-200 group-hover:text-zinc-800 dark:text-secondary-dark dark:group-hover:text-secondary-focus-dark">
+                    LoL Time
+                  </h3>
+                  <p className="text-sm text-quaternary duration-200 group-hover:text-zinc-700 dark:group-hover:text-secondary-dark">
+                    A League of Legends stat site which visualizes time spent in-game over the past week.
+                  </p>
+                </div>
+              </div>
+            </a>
+          </Link>
+
+          {/* Riot API */}
+          <a
+            href="https:///doc.deno.land/https://deno.land/x/riot_api/mod.ts"
+            target="_blank"
+            rel="noreferrer"
+            className="group"
+          >
+            <div className="flex items-center space-x-4">
+              <div className="w-1/4 p-2 text-secondary duration-200 group-hover:text-zinc-800 dark:text-secondary-dark dark:group-hover:text-secondary-focus-dark">
+                <icons.RiotAPI />
+              </div>
+              <div className="flex w-3/4 flex-col">
+                <h3 className="text-lg text-secondary duration-200 group-hover:text-zinc-800 dark:text-secondary-dark dark:group-hover:text-secondary-focus-dark">
+                  riot_api
+                </h3>
+                <p className="text-sm text-quaternary duration-200 group-hover:text-zinc-700 dark:group-hover:text-secondary-dark">
+                  A Deno library for easy interfacing with the Riot Games API.
+                </p>
+              </div>
+            </div>
+          </a>
+
           {/* The Quick Brown Fox */}
           <Link href="https://fox.subparprogramming.org" passHref>
             <a target="_blank" className="group">
